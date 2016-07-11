1c80d423b07f7138e229ecf7544565c64e622e0a
MadScientist <mberndtgen@gmail.com>
2016-07-12 00:16:02 +0200

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,92 @@ provider "aws" {
   region     = "${var.aws_region}"
 }
 
+/* 
+ * Chef Server AWS security group 
+ * https://docs.chef.io/server_firewalls_and_ports.html
+ */
+
+resource "aws_security_group" "chef-server" {
+  name        = "${var.hostname}.${var.domain} security group"
+  description = "Chef Server ${var.hostname}.${var.domain}"
+  vpc_id      = "${var.aws_vpc_id}"
+  tags = {
+    Name      = "${var.hostname}.${var.domain} security group"
+  }
+}
+
+
+# SSH
+
+resource "aws_security_group_rule" "chef-server_allow_22_tcp_allowed_cidrs" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["${split(",", var.allowed_cidrs)}"]
+  security_group_id = "${aws_security_group.chef-server.id}"
+}
+
+
+# HTTP (nginx)
+
+resource "aws_security_group_rule" "chef-server_allow_80_tcp" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.chef-server.id}"
+}
+
+
+# HTTPS (nginx)
+
+resource "aws_security_group_rule" "chef-server_allow_443_tcp" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.chef-server.id}"
+}
+
+
+# oc_bifrost (nginx LB)
+
+resource "aws_security_group_rule" "chef-server_allow_9683_tcp" {
+  type        = "ingress"
+  from_port   = 9683
+  to_port     = 9683
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.chef-server.id}"
+}
+
+
+# opscode-push-jobs
+
+resource "aws_security_group_rule" "chef-server_allow_10000-10003_tcp" {
+  type        = "ingress"
+  from_port   = 10000
+  to_port     = 10003
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.chef-server.id}"
+}
+
+
+# Egress: ALL
+
+resource "aws_security_group_rule" "chef-server_allow_egress" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.chef-server.id}"
+}
+
 
 /*
  * Local prep
@@ -158,6 +244,7 @@ resource "aws_instance" "chef-server" {
   # Push in cookbooks
   provisioner "remote-exec" {
     inline = [
+      "sudo knife ssl fetch",  
       "sudo knife cookbook upload -a -c .chef/knife.rb --cookbook-path /var/chef/cookbooks",
       "sudo rm -rf /var/chef/cookbooks",
     ]
@@ -204,7 +291,8 @@ resource "null_resource" "chef_chef-server" {
     log_to_file     = "${var.log_to_file}"
     node_name       = "${aws_instance.chef-server.tags.Name}"
     run_list        = ["recipe[chef-client::default]","recipe[chef-client::config]","recipe[chef-client::cron]","recipe[chef-client::delete_validation]","recipe[chef-server::default]","recipe[chef-server::addons]"]
-    server_url      = "https://${aws_instance.chef-server.tags.Name}/organizations/${var.org_short}"
+    # server_url      = "https://${aws_instance.chef-server.tags.Name}/organizations/${var.org_short}"
+    server_url      = "https://${aws_instance.chef-server.public_ip}/organizations/${var.org_short}"
     skip_install    = true
     validation_client_name = "${var.org_short}-validator"
     validation_key  = "${file("${module.validator.file}")}"
